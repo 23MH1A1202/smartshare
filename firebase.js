bde7f068f4cea5d783949ecf24bf68e7f02d9b24
Lalitha Sagar Ambati <23mh1a1202@acoe.edu.in>
2026-06-27 18:36:21 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,15 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore, doc, setDoc, getDoc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { firebaseConfig } from "./firebase-config.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBXBbEt_OEwOuHtiM3ERDcLwUZpXyNVtzM",
+    authDomain: "login-59720.firebaseapp.com",
+    projectId: "login-59720",
+    storageBucket: "login-59720.firebasestorage.app",
+    messagingSenderId: "598332882697",
+    appId: "1:598332882697:web:6f675adebeb816e64dddd8",
+    measurementId: "G-36F4WTT681"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
